627bb70a56e688178d90c0a66bbb118bb0b37991
Albert Pivnenko <pivnenko.albert@gmail.com>
2023-02-01 02:30:23 +0200

    created actions setting for config state

---
+++ src/app/store/actions/config.actions.ts
@@ -0,0 +1,19 @@
+import { Action } from "@ngrx/store";
+import { IConfig } from "src/app/models";
+
+export enum EConfigActions {
+    GetConfig = "[Config] Get config !",
+    GetConfigSuccess = "[Config] Get config Success !",
+}
+
+
+export class GetConfig implements Action {
+    public readonly type: string = EConfigActions.GetConfig;
+}
+
+export class GetConfigSuccess implements Action {
+    public readonly type: string = EConfigActions.GetConfigSuccess;
+    constructor(public payload: IConfig) { }
+}
+
+export type ConfigActions = GetConfig | GetConfigSuccess;
