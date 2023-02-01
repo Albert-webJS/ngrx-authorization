b3cd7d84fa9d4de787a8fc2e56ac81b8593230dd
Albert Pivnenko <pivnenko.albert@gmail.com>
2023-02-01 05:32:29 +0200

    add template in component. In bootstrapApplication add provide store

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,25 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { select, Store, StoreModule } from '@ngrx/store';
+import { GetConfig } from './store/actions';
+import { selectConfig } from './store/selectors';
+import { IAppState } from './store/state';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  imports: [CommonModule]
 })
-export class AppComponent {
-  title = 'ngrx-authorization';
+
+export class AppComponent implements OnInit {
+
+  constructor(private _store: Store<IAppState>) { }
+  public config$ = this._store.pipe(select(selectConfig))
+
+  ngOnInit(): void {
+    this._store.dispatch(new GetConfig())
+    console.log("store: ", this._store);
+  }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,9 @@
+import { provideHttpClient } from "@angular/common/http";
 import { bootstrapApplication } from "@angular/platform-browser";
 import { provideRouter, Routes } from "@angular/router";
+import { provideStore } from "@ngrx/store";
 import { AppComponent } from "./app/app.component";
+import { appReducers  } from "./app/store/reducers";
 
 
 const routes: Routes = []
@@ -8,6 +11,9 @@ const routes: Routes = []
 
 bootstrapApplication(AppComponent, {
   providers: [
-    provideRouter(routes)
-  ]
+    provideRouter(routes),
+    provideHttpClient(),
+    provideStore(appReducers),
+  ],
 }).catch(error => console.log(error));
+
