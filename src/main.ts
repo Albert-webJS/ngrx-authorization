831b505deb29a8c5a9901d0037c0be1d6b616ee3
Albert Pivnenko <pivnenko.albert@gmail.com>
2023-02-01 12:33:39 +0200

    replace in main.ts provider for store

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,10 @@
 import { provideHttpClient } from "@angular/common/http";
-import { importProvidersFrom } from "@angular/core";
 import { bootstrapApplication } from "@angular/platform-browser";
 import { provideRouter, Routes } from "@angular/router";
-import { EffectsModule } from "@ngrx/effects";
-import { StoreRouterConnectingModule } from "@ngrx/router-store";
+import { provideEffects } from "@ngrx/effects";
+import { provideRouterStore } from "@ngrx/router-store";
 import { provideStore } from "@ngrx/store";
-import { StoreDevtoolsModule } from "@ngrx/store-devtools";
+import { provideStoreDevtools  } from "@ngrx/store-devtools";
 import { AppComponent } from "./app/app.component";
 import { ConfigEffects } from "./app/store/effects/config.effects";
 import { UserEffects } from "./app/store/effects/user.effects";
@@ -17,14 +16,12 @@ const routes: Routes = []
 
 bootstrapApplication(AppComponent, {
   providers: [
-    provideRouter(routes),
     provideHttpClient(),
+    provideRouter(routes),
     provideStore(appReducers),
-    importProvidersFrom(
-      StoreRouterConnectingModule.forRoot(),
-      StoreDevtoolsModule.instrument(),
-      EffectsModule.forRoot([ConfigEffects, UserEffects])
-    )
+    provideRouterStore(),
+    provideEffects([ConfigEffects, UserEffects]),
+    provideStoreDevtools(),
   ],
 }).catch(error => console.log(error));
 
