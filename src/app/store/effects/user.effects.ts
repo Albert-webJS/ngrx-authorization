903b76747c974ffe5f1dd9d796eec68fd4b56c6b
Albert Pivnenko <pivnenko.albert@gmail.com>
2023-02-01 04:32:55 +0200

    add user and config effects for app

--- src/app/store/effects/user.effects.ts
+++ src/app/store/effects/user.effects.ts
@@ -1,9 +1,11 @@
 import { Injectable } from "@angular/core";
 import { Actions, createEffect, ofType } from "@ngrx/effects";
-import { Store } from "@ngrx/store";
-import { withLatestFrom } from "rxjs";
+import { select, Store } from "@ngrx/store";
+import { map, of, switchMap, withLatestFrom } from "rxjs";
+import { IUserHttp } from "src/app/models/http-models/user-http.intarface";
 import { UserService } from "src/app/services/user.service";
-import { EUserActions, GetUser } from "../actions";
+import { EUserActions, GetUser, GetUsers, GetUsersSuccess, GetUserSuccess } from "../actions";
+import { selectUserList } from "../selectors";
 import { IAppState } from "../state";
 
 
@@ -18,4 +20,19 @@ export class UserEffects {
         private _store: Store<IAppState>
     ) { }
 
+    getUser$ = createEffect(() => this._actions$.pipe(
+        ofType<GetUser>(EUserActions.GetUser),
+        map(action => action.payload),
+        withLatestFrom(this._store.pipe(select(selectUserList))),
+        switchMap(([id, users]) => {
+            const selectedUser = users?.filter(user => user.id === +id)[0];
+            return of(new GetUserSuccess(selectedUser!));
+        })
+    ))
+
+    getUsers$ = createEffect(() => this._actions$.pipe(
+        ofType<GetUsers>(EUserActions.GetUsers),
+        switchMap(() => this._userSerivice.getUsers()),
+        switchMap((userHttp: IUserHttp) => of(new GetUsersSuccess(userHttp.users)))
+    ))
 }
