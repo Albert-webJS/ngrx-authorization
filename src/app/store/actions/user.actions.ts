532e74ce6e9f0558f1ea38aff10c6d54f5110a45
Albert Pivnenko <pivnenko.albert@gmail.com>
2023-02-01 02:23:48 +0200

    describe user action classes in actions folder

---
+++ src/app/store/actions/user.actions.ts
@@ -0,0 +1,33 @@
+import { Action } from "@ngrx/store";
+import { IUser } from "src/app/models";
+
+
+export enum EUserActions {
+    GetUsers = '[User] Get Users',
+    GetUsersSuccess = '[User] Get Users Success',
+    GetUser = '[User] Get User!',
+    GetUserSuccess = "[User] Get User Succes!",
+}
+
+
+export class GetUsers implements Action {
+    public readonly type: string = EUserActions.GetUser;
+}
+
+export class GetUsersSuccess implements Action {
+    public readonly type: string = EUserActions.GetUsersSuccess;
+    constructor(public payload: IUser[]) { }
+}
+
+export class GetUser implements Action {
+    public readonly type: string = EUserActions.GetUser;
+    constructor(public payload: number) { }
+}
+
+export class GetUserSuccess implements Action {
+    public readonly type: string = EUserActions.GetUserSuccess;
+    constructor(public payload: IUser) { }
+}
+
+
+export type UserAcions = GetUser | GetUserSuccess | GetUsers | GetUsersSuccess
